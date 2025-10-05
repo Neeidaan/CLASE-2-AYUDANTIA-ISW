6fd40b78c00a47cdf309d345ddd22ddecac2be34
Neeidaan <neidanmartinez22@gmail.com>
2025-10-04 21:08:58 -0300

    Actualizo index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
-import { AppDataSource, connectDB } from "./config/configDB.js";
+import { AppDataSource, connectDB } from "./config/configDb.js";
 import { routerApi } from "./routes/index.routes.js";
+import {HOST,PORT} from "./config/configEnv.js"
 
 const app = express();
 app.use(express.json());
@@ -12,16 +13,15 @@ app.get("/", (req, res) => {
   res.send("¡Bienvenido a mi API REST con TypeORM!");
 });
 
+
 // Inicializa la conexión a la base de datos
 connectDB()
   .then(() => {
     // Carga todas las rutas de la aplicación
     routerApi(app);
 
-    // Levanta el servidor Express
-    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-      console.log(`Servidor iniciado en http://localhost:${PORT}`);
+      console.log(`Servidor iniciado en ${HOST}:${PORT}`);
     });
   })
   .catch((error) => {
